58173039cbe9975a14f34d3eebcb5e7af0af526b
estie <estie@estienoMacBook-Pro-3.local>
2021-07-23 12:54:37 +0900

    feat: create-calendar_components

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <HelloWorld message="Hello World!" />
+    <!-- <HelloWorld message="Hello World!" /> -->
+    <Calendar />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+// import HelloWorld from './components/HelloWorld.vue';
+import Calendar from './components/Calendar.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    // HelloWorld
+    Calendar
   },
 };
 </script>
