d05b5071bb19f769a66fae2e710486afdf4a3a64
estie <estie@estienoMacBook-Pro-3.local>
2021-07-23 13:57:25 +0900

    feat: setup-axios

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -3,15 +3,30 @@
     <h1>Calendar</h1>
     <p>events:</p>
     <p>{{ events }}</p>
-    <button type="submit">fetchEvents</button>
+    <button type="submit" @click="fetchEvents()">fetchEvents</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Calendar',
   data: () => ({
     events: []
   }),
+  methods: {
+    fetchEvents() {
+      // GETリクエストを送信し、取得データをevents変数に代入する
+      axios
+        .get('http://localhost:3000/events')
+        .then(response => {
+          this.events = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }
 };
 </script>
