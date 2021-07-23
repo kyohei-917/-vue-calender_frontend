573b9f790d525950e287038308b0879eb9c741a4
estie <estie@estienoMacBook-Pro-3.local>
2021-07-23 12:27:24 +0900

    feat: prittier-setup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
-    <p>{{ message }}</p>
+    <!-- <p>{{ message }}</p> -->
+    <p>Hello World!</p>
+    <p>Hello World! Hello World! Hello World! Hello World! Hello World! Hello World!</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
-  props: ["message"], // 親コンポーネントからmessageを受け取る
+  name: 'HelloWorld',
+  // props: ["message"], // 親コンポーネントからmessageを受け取る
 };
 </script>
